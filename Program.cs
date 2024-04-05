e775586b098b72ab2735f8325ac8c6e87573b9d6
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-05 01:22:54 -0300

    Implementacao ForEach

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 arrayInteiros[3] = 1;
 
+
+Console.WriteLine("percorrendo o array com o For");
 for(int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("\npercorrendo o array com o Foreach");
+int contadorForeach = 0;
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+    contadorForeach++;
 }
