3390e88a12a919141bf2c6c3cebf225b3520f640
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-05 01:07:30 -0300

    Implementando um array de inteiros

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+int[] arrayInteiros = new int[4];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+arrayInteiros[3] = 1;
+
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+}
